215ada85c00851daea43ff68713faf06f1bf55d2
Az-Back <back.ranged@gmail.com>
2022-02-09 15:19:24 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+window.addEventListener('load', function(){
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 800;
+    canvas.height = 720;
+
+    class InputHandler {
+        constructor(){
+            this.keys = [];
+            window.addEventListener('keydown', e => {
+                if ((e.key === "ArrowDown" || e.key === 'ArrowUp'
+                
+                
+                    && this.keys.indexOf(e.key) === -1) {
+                    this.keys.push(e.key);
+                }
+                console.log(e.key, this.keys);
+            });
+            window.addEventListener('keydown', e => {
+                if (e.key === "ArrowUp") {
+                    this.keys.splice(this.keys.indexOf(e.key), 1);
+                }
+                console.log(e.key, this.keys);
+            });    
+        }
+    }
+
+    class Player {
+
+    }
+
+    class Background {
+
+    }
+
+    class Enemy {
+
+    }
+
+    function handleEnemies(){
+
+    }
+
+    function displayStatusText(){
+
+    }
+
+    const input = new InputHandler();
+
+    function animate(){
+
+    }
+});
